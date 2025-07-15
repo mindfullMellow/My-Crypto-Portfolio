bdc6c1f94a5fccdd463aafc4bf1369b36c830eef
Daniel Samuel <samueldaniel4198@gmail.com>
2025-07-15 15:17:33 +0100

    ADD: Assets section wascreated and added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ export default {
         "border-color": "#2d372a",
         hover: "#0a7121",
         "text-color-1": "#a5b6a0",
+        "table-color": "#42513e",
+        "table-bg": "#131712",
+        "table-bg-2": "#1f251d",
+        "tabel-top-border": "#42513e",
+        "change-red": "#da0b35",
+        "change-green": "#0bda35",
       },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
