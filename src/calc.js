15223e10991be14d78b72de2d3ba84b283657f46
Daniel Samuel <samueldaniel4198@gmail.com>
2025-07-26 22:05:27 +0100

    ADD: calulator page and layout

---
+++ src/calc.js
@@ -0,0 +1,77 @@
+"use strict";
+
+import "./main.css";
+
+///////////////////////////////////////////////////////////////////////
+// code to dynamically implement the mobile nav logic
+
+const buttonEL = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header-sm");
+const profileMobile = document.querySelector("#mobile-profile");
+const mainNavEL = document.querySelector(".main-nav");
+
+buttonEL.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// intersection observer for automatically closing nav-open
+const obsMainNav = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    // close the nav-open when user leaves it visible section
+    if (
+      ent.isIntersecting === false &&
+      headerEl.classList.contains("nav-open")
+    ) {
+      headerEl.classList.remove("nav-open");
+    }
+  },
+
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+// calling the intersection functions
+obsMainNav.observe(mainNavEL);
+
+///////////////////////////////////////////////////////////////////////
+// MODAL logic
+const overlayEL = document.querySelector(".overlay");
+const zIndex = document.querySelector(".btn-mobile-nav");
+const profileBtn = document.querySelectorAll(".profile-btn");
+const profileModal = document.querySelectorAll(".profile-modal");
+
+///////////////////////////////////////////////
+//storing functions
+const openProfileModal = function () {
+  overlayEL.classList.remove("hidden");
+  zIndex.classList.remove("z-50");
+  for (let i = 0; i < profileModal.length; i++) {
+    profileModal[i].classList.replace("hidden", "flex");
+  }
+};
+
+const closeProfileModal = function (e) {
+  if (e.target === overlayEL) {
+    overlayEL.classList.add("hidden");
+    zIndex.classList.add("z-50");
+    for (let i = 0; i < profileModal.length; i++) {
+      profileModal[i].classList.replace("flex", "hidden");
+    }
+  }
+};
+
+//show modal when the profile is clicked
+for (let i = 0; i < profileBtn.length; i++) {
+  profileBtn[i].addEventListener("click", openProfileModal);
+}
+
+// close modal when overlay is clicked
+overlayEL.addEventListener("click", closeProfileModal);
+
+console.log("this works");
